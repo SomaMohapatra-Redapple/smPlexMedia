b931cef0beb416af30ff70b344c7f722bdcfbe09
injamamul-lvcd <injamamul143.cob@gmail.com>
2023-09-13 17:05:12 +0530

    initial setup for microgaming service.server,database,app are ready

---
+++ services/microgaming/src/routes/router.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const appConfig = require("../../config/appConfig");
+const auth = require('../middlewares/auth');
+// const validator = require('../../middlewares/pp/ppValidator');
+// const { rateLimiter, rateLimiterByIP } = require('../../middlewares/rateLimiter');
+// const ppSlotController = require("../../controllers/pp/ppSlotController");
+
+
+module.exports.setRouter = (app) => {
+
+  let baseUrl = `${appConfig.apiVersion}/microgaming/slot`;
+
+  //app.post(`${baseUrl}/:function`,validator.ppReqValidator, ppSlotController.handler);
+};
+
