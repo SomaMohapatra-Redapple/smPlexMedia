deb820e7d102f4ed8ac4c97753f0f562db043519
SomaMohapatra-Redapple <soma.redappletech.com>
2023-09-15 18:55:17 +0530

    minor modification

--- services/hq/src/routes/client.js
+++ services/hq/src/routes/client.js
@@ -2,7 +2,6 @@ const path = require("path");
 const clientController = require("../controllers/client");
 const appConfig = require("../../config/appConfig");
 const auth = require("../middlewares/auth");
-const validator = require("../middlewares/validator");
 
 module.exports.setRouter = (app) => {
   let baseUrl = `${appConfig.apiVersion}`;
