2f1e63096ca08545815f155ff703968cb971cc9a
Akash-Redapple <akash.paul@redappletech.com>
2023-09-15 16:46:10 +0530

    extra work

--- services/pp/www/rest/server.js
+++ services/pp/www/rest/server.js
@@ -5,6 +5,7 @@ const startServer = (app) => {
     const server = http.createServer(app);
 
     server.listen(process.env.REST_PORT);
+    console.log(`Server is Listning on port : ${process.env.REST_PORT}`);
 
     server.on('error', (err) => {
         console.log(`Error : ${err}`);
