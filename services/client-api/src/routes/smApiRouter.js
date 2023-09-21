eaae3603cade066aabfe03adf4eb08072e06bdaa
Akash-Redapple <akash.paul@redappletech.com>
2023-09-21 15:53:01 +0530

    balance API changes

--- services/client-api/src/routes/smApiRouter.js
+++ services/client-api/src/routes/smApiRouter.js
@@ -1,11 +1,12 @@
 const appConfig = require("../../config/appConfig");
 const smApiController = require('../controller/smApiController');
+const validator = require('../middlewares/validator/validator');
 
 
 module.exports.setRouter = (app) => {
 
   let baseUrl = `${appConfig.apiVersion}/client-api`;
 
-  app.get(`${baseUrl}/user-balance`, smApiController.userBalance);
+  app.post(`${baseUrl}/user-balance`, validator.apiValidator, smApiController.userBalance);
 };
 
