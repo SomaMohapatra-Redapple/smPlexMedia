7c0589dc564e753d1a622b13929985e5bc4ee798
SomaMohapatra-Redapple <soma.redappletech.com>
2023-10-05 18:33:42 +0530

    Added role to log in for super admin and admin login api

--- services/hq/src/controllers/admin.js
+++ services/hq/src/controllers/admin.js
@@ -34,15 +34,18 @@ const add_admin = async (req, res, next) => {
   }
 };
 
+//admin_login
+
 const admin_login = async (req, res, next) => {
   try {
-    const { user_name, password } = req.body;
+    const { user_name, password,role } = req.body;
     const query = { user_name: user_name };
     const admin = await FindAdmin(query);
+    if(admin){
     console.log("req.connection", req.connection.remoteAddress);
     console.log("req.body", req.body);
     console.log("admin", admin);
-    if (admin.user_name == user_name && admin.password == password) {
+    if (role == "admin" && admin.user_name == user_name && admin.password == password) {
       token = jwt.sign(
         { id: admin.id, user_name: admin.user_name },
         process.env.ENC_KEY,
@@ -57,6 +60,44 @@ const admin_login = async (req, res, next) => {
         message: "you are not the admin",
       });
     }
+      
+
+    }
+    else{
+      res.status(400).send({
+        message : "there is no such admin in admin table"
+      })
+    }
+    
+  } catch (e) {
+    console.log("error", e);
+  }
+};
+
+//superadmin_login
+const superadmin_login = async (req, res, next) => {
+  try {
+    const { user_name, password,role } = req.body;
+    const query = { user_name: user_name };
+    const admin = await FindAdmin(query);
+    console.log("req.connection", req.connection.remoteAddress);
+    console.log("req.body", req.body);
+    console.log("admin", admin);
+    if (role == "superadmin" && admin.user_name == user_name && admin.password == password) {
+      token = jwt.sign(
+        { id: admin.id, user_name: admin.user_name },
+        process.env.ENC_KEY,
+        { expiresIn: process.env.JWT_TOKEN_EXPIRE_TIME }
+      );
+      res.status(200).send({
+        message: "super admin has logged in successfully",
+        token: token,
+      });
+    } else {
+      res.status(400).send({
+        message: "you are not the super admin",
+      });
+    }
   } catch (e) {
     console.log("error", e);
   }
@@ -65,4 +106,5 @@ const admin_login = async (req, res, next) => {
 module.exports = {
   add_admin: add_admin,
   admin_login: admin_login,
+  superadmin_login : superadmin_login
 };
