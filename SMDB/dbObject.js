84949e0013950a97797511524c113db5fde05d89
SomaMohapatra-Redapple <soma.redappletech.com>
2023-10-06 17:37:52 +0530

    added created_at and updated_at in smdb

--- SMDB/dbObject.js
+++ SMDB/dbObject.js
@@ -145,14 +145,6 @@ let smObj = {
             type: String,
             default: null
         },
-        created_at: {
-            type: Date,
-            default: ""
-        },
-        updated_at: {
-            type: Date,
-            default: ""
-        },
         created_at: timeLib.currentDateTime(),
         updated_at: timeLib.currentDateTime()
     },
