16f5e564434b62573b5a5b0883ab0bc2acbc0270
Rajdeep-Redapple <rajdeep@redappletech.com>
2023-10-12 11:21:28 +0530

    configured api-server for initial load

--- services/api-server/www/rest/server.js
+++ services/api-server/www/rest/server.js
@@ -1,15 +1,15 @@
 const http = require('http');
-const ws = require('../socket/ws');
+// const ws = require('../socket/ws');
 
 const startServer = (app) => {
     const server = http.createServer(app);
 
     server.listen(process.env.REST_PORT);
 
-    // server.on('listening',()=>{
-    //     console.log(`server listening on port : ${server.address().port}`);
-    //     ws.startSocket(server);
-    // });
+    server.on('listening',()=>{
+        console.log(`server listening on port : ${server.address().port}`);
+        // ws.startSocket(server);
+    });
 
     server.on('error', (err) => {
         console.log(`Error : ${err}`);
