bac30ba28daa92969bf458ac3d847f3bfd18bc00
SomaMohapatra-Redapple <soma.redappletech.com>
2023-10-26 15:59:45 +0530

    cors changed

--- services/hq/src/controllers/client.js
+++ services/hq/src/controllers/client.js
@@ -327,6 +327,32 @@ catch(e){
 }
 }
 
+//edit client
+let edit_client = async(req,res,next) => {
+  try {
+    req.body.client_name = req.body.firstname + " " + req.body.lastname;
+    const update_account = await ClientTable.updateOne(
+      { _id: req.body._id }, // Specify the filter to match the document
+      { $set: { client_name: req.body.client_name, email: req.body.email, contact: req.body.contact, username: req.body.username, currency: req.body.currency } } // Specify the update operation
+    );
+
+    console.log("updated_account", update_account);
+    if (update_account) {
+      res.status(200).send({
+        updated_account: update_account,
+        message: "account details updated",
+      });
+    } else {
+      res.status(400).send({
+        updated_account: update_account,
+        message: "account details could not updated",
+      });
+    }
+  } catch (e) {
+    console.log("error from update_account", e);
+  }
+}
+
 // let all_client = async (req,res,next) =>{
 //   try{
 //     const agg = await AdminTable.aggregate([
@@ -765,5 +791,6 @@ module.exports = {
   log_in: log_in,
   add_client_by_client: add_client_by_client,
   nested_client: nested_client,
+  edit_client : edit_client,
   search_client_user : search_client_user
 };
--- services/hq/src/routes/client.js
+++ services/hq/src/routes/client.js
@@ -10,7 +10,8 @@ module.exports.setRouter = (app) => {
    app.post(`${baseUrl}/add_client`,auth.isAuthorized,validation.addClient,clientController.add_client);
    app.post(`${baseUrl}/all_client`,validation.showAllClient,auth.isAuthorized,clientController.all_client);
    app.post(`${baseUrl}/log_in`,validation.loginValidate,clientController.log_in);
-   app.post(`${baseUrl}/nested_client`,clientController.nested_client);
+   app.post(`${baseUrl}/edit_client`,validation.editClientValidate,clientController.edit_client);
+   //app.post(`${baseUrl}/nested_client`,clientController.nested_client);
    //app.post(`${baseUrl}/add_client_by_client`,auth.isAuthorized,validation.addClient,clientController.add_client_by_client);
    //app.post(`${baseUrl}/add_client_by_client`,auth.isAuthorized,validation.addClient,clientController.add_client_by_client);
 };
