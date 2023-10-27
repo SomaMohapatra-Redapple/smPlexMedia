358fc52d3cd3e55294570933659a36fc6f853408
SomaMohapatra-Redapple <soma.redappletech.com>
2023-10-27 12:06:48 +0530

    account api completed

--- services/hq/src/routes/account.js
+++ services/hq/src/routes/account.js
@@ -5,7 +5,7 @@ const validation = require("../middlewares/validator")
 //const rateLimit = require("../middlewares/rateLimiter");
 
 module.exports.setRouter = (app) => {
-  let baseUrl = `${appConfig.apiVersion}`;
+  let baseUrl = `${appConfig.apiVersion}/account`;
 //   app.post (`${baseUrl}/withdraw_balance`,auth.isAuthorized,betController.withdraw_balance);
    app.post(`${baseUrl}/add_account`,validation.addAccountValidation,auth.isAuthorized,clientController.add_account);
    //app.post(`${baseUrl}/add_account_techicals`,validation.addAccountTechnicalValidation,auth.isAuthorized,clientController.add_account_techicals);
