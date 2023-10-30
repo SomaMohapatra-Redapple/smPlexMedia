f32f48f3d84cd3f5813702584a84dadf24a2ac56
Injamamul Hoque <144695476+Injamamul-Redapple@users.noreply.github.com>
2023-10-30 16:10:55 +0530

    Superadmin api game (#116)
    * search user/client integrated
    
    * account api
    
    * Creating add_account api
    Update account api
    
    * cors pkg removed
    
    * field values are made required,cors removed
    
    * did the suggested changes with show all account api added
    
    * cors changed
    
    * cors changes done with client edit,password edit api added
    
    * cors removed
    
    * delete client api
    
    * cors modify
    
    * authorization added to app.js and routelogger.js
    
    * account api completed
    
    * extra log_in api has been removed
    
    * show_lang_list,category_list,add_category,edit_category,delete_category apis integrated
    
    * category completed
    
    * category list pagination updated
    
    * cors error checking
    
    * cors errors solving
    
    ---------
    
    Co-authored-by: SomaMohapatra-Redapple <soma.redappletech.com>

--- services/hq/app.js
+++ services/hq/app.js
@@ -39,11 +39,8 @@ app.use(routeLoggerMiddleware.logIp);
 
 app.all(appConfig.allowedCorsOrigin, function (req, res, next) {
   res.header("Access-Control-Allow-Origin", appConfig.allowedCorsOrigin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,token,key"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,token,key,Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   next();
 });
 process.on("unhandledRejection", (reason, p) => {
