ac2dd2cbd7f4d6e4702fea2cb719a1c0428e1b8f
Injamamul Hoque <144695476+Injamamul-Redapple@users.noreply.github.com>
2023-10-31 17:33:44 +0530

    Sm db (#122)
    * added two schema for client_db & updated transaction table
    
    * updated dbObject - added country and jurisdiction
    
    * updated dbObject - added country and jurisdiction
    
    * account_name will be in account table...
    And
    enviornment will be in account_technicals table changed
    
    * new client transction changes
    
    * language table added,two filed(category_name,show_in_lobby)added in category table.
    
    * data type changed of category_name filed
    
    * sm_db updated with a new table Reports
    
    * sm_db updated
    
    ---------
    
    Co-authored-by: Rajdeep-Redapple <rajdeep@redappletech.com>
    Co-authored-by: SomaMohapatra-Redapple <soma.redappletech.com>
    Co-authored-by: Akash-Redapple <akash.paul@redappletech.com>

--- SMDB/dbObject.js
+++ SMDB/dbObject.js
@@ -88,6 +88,10 @@ let smObj = {
             type: Object,
             required: true
         },
+        category_name: {
+            type: Object,
+            required: true
+        },
         category_icon: {
             type: String,
             required: true,
@@ -167,6 +171,10 @@ let smObj = {
         }
     },
     Game: {
+        module_id:{
+            type:String,
+            isObjectId:true
+        },
         game_provider_id: {
             type: String,
             ref: 'providers',
@@ -277,6 +285,10 @@ let smObj = {
         }
     },
     Provider: {
+        module_id:{
+            type:String,
+            isObjectId:true
+        },
         game_provider_name: {
             type: String,
             required: true,
@@ -595,6 +607,52 @@ let smObj = {
             default: ""
         }
 
+    },
+    Report:{
+        account_id: {
+            type: String,
+            required: true,
+            isObjectId: true
+        },
+        category_id: {
+            type: String,
+            required: true,
+            isObjectId: true
+        },
+        provider_id: {
+            type: String,
+            required: true,
+            isObjectId: true
+        },
+        module_id: {
+            type: String,
+            required: true,
+            isObjectId: true
+        },
+        total_bet:{
+            type: Number,
+            required: true
+        },
+        total_winning:{
+            type: Number,
+            required: true
+        },
+        total_margin:{
+            type: Number,
+            required: true
+        },
+        total_for_day:{
+            type: Date,
+            required: true
+        },
+        created_at: {
+            type: Date,
+            default: ""
+        },
+        updated_at: {
+            type: Date,
+            default: ""
+        }
     },
     Client_db_users: {
         name: {
